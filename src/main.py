7712188d88d679d924af57031684a32991dea689
Cezar Bulancea <118464357+Cezarrr9@users.noreply.github.com>
2024-01-20 17:26:03 +0100

    modify env + created 2 classes + suggestions

--- src/main.py
+++ src/main.py
@@ -1,2 +1,8 @@
+# Here we have to decide how are we going to conduct the experiments 
+
+# Either we are going to have some experiments in the main file and that's all or 
+# we're going to have different file for each experiment and be more precise
+# about what they re doing in the title 
+
 if __name__ == '__main__':
     pass # You can delete this line :)
