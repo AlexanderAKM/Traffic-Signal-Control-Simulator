64e832f445c93505f47790a153ba4d8549fe7ac1
AlexanderAKM <a.k.muller.2@student.rug.nl>
2024-01-28 11:54:22 +0100

    stochastic agent inherits from agent.py

--- src/Agents/stochastic.py
+++ src/Agents/stochastic.py
@@ -1,3 +1,35 @@
+from agent import Agent  # Make sure this import matches the location of your Agent class
+
+class StochasticModel(Agent):
+    def __init__(self, num_experiments=1):
+        super().__init__('Stochastic', num_experiments)
+
+    def setup_model(self, env):
+        self.env = env
+        self.action_space = env.action_space
+        self.model = self  # In this case, the model is the StochasticModel itself
+
+    def learn(self, total_timesteps: int):
+        obs = self.env.reset()
+        for step in range(total_timesteps):
+            action = self.action_space.sample()  # Random action
+            obs, reward, terminated, truncated, info = self.env.step(action)
+            if truncated:  # Check if the episode should end
+                self.env.save_csv(self.env.out_csv_name, self.env.episode)  # Call the save_csv method
+                break
+        self.env.close()
+
+    def predict(self, observation, state=None, mask=None, deterministic=False):
+        return self.action_space.sample(), None
+
+# Usage
+stochastic_agent = StochasticModel()
+stochastic_agent.run_experiments()
+
+
+
+
+'''
 import os
 import sys
 import json
@@ -54,3 +86,4 @@ for i in range(num_experiments):
     
     # After running the model, you may want to save the results
     # Save results code here (if needed)
+'''
