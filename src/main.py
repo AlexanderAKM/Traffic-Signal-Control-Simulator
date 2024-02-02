91b8f26036705cd3ef7ec783d3c54f1def94b289
Cezar Bulancea <118464357+Cezarrr9@users.noreply.github.com>
2024-02-02 18:42:17 +0100

    combining all the graphs into one

--- src/main.py
+++ src/main.py
@@ -26,24 +26,24 @@ if __name__ == '__main__':
     # DQN
     for i in range(1):
         env = SumoEnvironment(
-            net_file="src/Intersection/2way-single-intersection/single-intersection.net.xml",
-            route_file="src/Intersection/2way-single-intersection/single-intersection-vhvh.rou.xml",
-            out_csv_name=f"data/dqn_2way_test_csv_run{i}",
-            use_gui=True,
-            num_seconds=6000,
+            net_file = "src/Intersection/2way-single-intersection/single-intersection.net.xml",
+            route_file = "src/Intersection/2way-single-intersection/single-intersection-vhvh.rou.xml",
+            out_csv_name = f"data/DQN_2way_run{i}",
+            use_gui = False,
+            num_seconds = 6000,
         )
 
         dqn = DQN(env = env)
-        dqn.train(num_episodes=1)
+        dqn.train(num_episodes = 1)
 
     # A2C
     for i in range(1):
         env = SumoEnvironment(
-            net_file="src/Intersection/2way-single-intersection/single-intersection.net.xml",
-            route_file="src/Intersection/2way-single-intersection/single-intersection-vhvh.rou.xml",
-            out_csv_name=f"data/A2C_2way_test_csv_run{i}",
-            use_gui=True,
-            num_seconds=6000,
+            net_file = "src/Intersection/2way-single-intersection/single-intersection.net.xml",
+            route_file = "src/Intersection/2way-single-intersection/single-intersection-vhvh.rou.xml",
+            out_csv_name = f"data/A2C_2way_run{i}",
+            use_gui = False,
+            num_seconds = 6000,
         )
 
         a2c = A2C(env = env)
--- src/plotting/plot.py
+++ src/plotting/plot.py
@@ -1,84 +1,32 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load the CSV file
-#file_path = 'data/2way_test_csv_run0_conn0_ep1.csv'
-#file_path = 'data/dqn_conn0_ep1.csv'  
-#file_path = 'data/DQN_2way_test_csv_run0_conn0_ep1.csv'
-#file_path = 'data/A2C_2way_test_csv_run0_conn0_ep1.csv'
-# file_path = 'data/PPO_2way_test_csv_run0_conn0_ep1.csv'
-# file_path = 'data/dqn_2way_test_csv_run0_conn0_ep2.csv'
-file_path = 'data/A2C_2way_test_csv_run0_conn0_ep1.csv'
-data = pd.read_csv(file_path)
+# Load the CSV files
+stochastic_file = 'data/2way_stochastic_csv_run0_conn0_ep1.csv'
+ppo_file = 'data/PPO_2way_test_csv_run0_conn0_ep1.csv'
+dqn_file = 'data/dqn_2way_test_csv_run0_conn0_ep2.csv'
+a2c_file = 'data/A2C_2way_test_csv_run0_conn0_ep2.csv'
+
+# Read the csv files
+dqn_data = pd.read_csv(dqn_file)
+a2c_data = pd.read_csv(a2c_file)
+ppo_data = pd.read_csv(ppo_file)
+stochastic_data = pd.read_csv(stochastic_file)
 
 # Plotting
 plt.figure(figsize=(10, 5))
-plt.plot(data['step'], data['system_total_waiting_time'], label='Total Waiting Time')
+plt.plot(dqn_data['step'], dqn_data['system_total_waiting_time'], label='DQN')
+plt.plot(a2c_data['step'], a2c_data['system_total_waiting_time'], label='A2C')
+plt.plot(ppo_data['step'], ppo_data['system_total_waiting_time'], label='PPO')
+plt.plot(stochastic_data['step'], stochastic_data['system_total_waiting_time'], label='Random')
 
+# Label the axis 
 plt.xlabel('Step')
 plt.ylabel('System Total Waiting Time')
 plt.title('System Total Waiting Time vs Step')
 plt.legend()
 plt.grid(True)
 
-plt.savefig('outputs/a2c_2way_intersection.png', format='png')
+plt.savefig('outputs/total_waiting_time.png', format='png')
 
 plt.show()
-
-
-
-
-
-
-
-
-'''
-import pandas as pd
-import matplotlib.pyplot as plt
-import glob
-
-# Define a function to calculate the rolling average
-def rolling_average(data, window_size=2):
-    return data.rolling(window=window_size, min_periods=1).mean()
-
-# File pattern for the CSV files
-file_pattern = 'data/test_csv_run*.csv'
-files = glob.glob(file_pattern)
-
-# Initialize a DataFrame to hold all the data
-summed_data = None
-
-# Loop through all files and sum the waiting times
-for file in files:
-    df = pd.read_csv(file)
-    if summed_data is None:
-        summed_data = df['system_total_waiting_time']
-    else:
-        summed_data += df['system_total_waiting_time']
-
-# Calculate the rolling average for the summed data
-summed_data_ra = rolling_average(summed_data, window_size=2)  # Adjust window size as needed
-
-# Plotting
-plt.figure(figsize=(14, 7))  # Adjust figure size to your preference
-plt.plot(df['step'], summed_data_ra, label='Total Waiting Time (Smoothed)')
-
-# Labeling the axes
-plt.xlabel('Step')
-plt.ylabel('Summed System Total Waiting Time (Smoothed)')
-
-# Title for the plot
-plt.title('Smoothed System Total Waiting Time vs. Time')
-
-# Display legend
-plt.legend()
-
-# Show grid
-plt.grid(True)
-
-# Save the plot as a file (optional)
-plt.savefig('data/smoothed_total_waiting_time_plot.png', format='png')
-
-# Show the plot
-plt.show()
-'''
