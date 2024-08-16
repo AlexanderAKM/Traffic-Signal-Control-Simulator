e350f937f7d50ce2d8f8b062019c7e9314cea0c4
AlexanderAKM <a.k.muller.2@student.rug.nl>
2024-08-16 10:37:08 +0200

    Changed some minor things

--- src/main.py
+++ src/main.py
@@ -2,25 +2,33 @@ import os
 import sys
 import json
 
-# Read configuration (see config.json)
-with open('config.json', 'r') as config_file:
+# Load configuration from config_mine.json
+with open('config_mine.json', 'r') as config_file:
     config = json.load(config_file)
 
-if "SUMO_HOME" in os.environ:
-    tools = os.path.join(os.environ["SUMO_HOME"], "tools")
+# Set SUMO tools path from config file
+if config["sumo_tools_path"]:
+    tools = config["sumo_tools_path"]
     sys.path.append(tools)
 else:
-    sys.exit("Please declare the environment variable 'SUMO_HOME'")
+    if "SUMO_HOME" in os.environ:
+        tools = os.path.join(os.environ["SUMO_HOME"], "tools")
+        sys.path.append(tools)
+    else:
+        sys.exit("Please declare the environment variable 'SUMO_HOME' or set 'sumo_tools_path' in the config file.")
+
 import traci
 
-# Use paths from config file
-sys.path.append(config["project_base_path"])
-from src.environment.env import SumoEnvironment
+# Use project base path from config file
+if config["project_base_path"]:
+    sys.path.append(config["project_base_path"])
+else:
+    sys.exit("Please set 'project_base_path' in the config file.")
 
+from src.environment.env import SumoEnvironment
 from src.agents.dqn import DQN
 from src.agents.a2c import A2C
 from src.agents.stochastic import StochasticModel
-
 from src.plotting.plot import plotWaitingTime
 import numpy as np
 import pandas as pd
@@ -34,10 +42,10 @@ if __name__ == '__main__':
     
     # DQN
     env = SumoEnvironment(
-        net_file = "src/Intersection/2way-single-intersection/single-intersection.net.xml",
-        route_file = "src/Intersection/2way-single-intersection/single-intersection-vhvh.rou.xml",
-        out_csv_name = f"data/DQN_2way",
-        use_gui = False,
+        net_file = os.path.join(config["project_base_path"], "src/Intersection/2way-single-intersection/single-intersection.net.xml"),
+        route_file = os.path.join(config["project_base_path"], "src/Intersection/2way-single-intersection/single-intersection-vhvh.rou.xml"),
+        out_csv_name = os.path.join(config["project_base_path"], "data/DQN_2way"),
+        use_gui = True,
         num_seconds = 10000,
     )
 
@@ -46,10 +54,10 @@ if __name__ == '__main__':
 
     # A2C
     env = SumoEnvironment(
-        net_file = "src/Intersection/2way-single-intersection/single-intersection.net.xml",
-        route_file = "src/Intersection/2way-single-intersection/single-intersection-vhvh.rou.xml",
-        out_csv_name = f"data/A2C_2way",
-        use_gui = False,
+        net_file = os.path.join(config["project_base_path"], "src/Intersection/2way-single-intersection/single-intersection.net.xml"),
+        route_file = os.path.join(config["project_base_path"], "src/Intersection/2way-single-intersection/single-intersection-vhvh.rou.xml"),
+        out_csv_name = os.path.join(config["project_base_path"], "data/A2C_2way"),
+        use_gui = True,
         num_seconds = 10000,
     )
 
@@ -58,8 +66,3 @@ if __name__ == '__main__':
 
     # Plot the results
     plotWaitingTime()
-
-
-
-
-
